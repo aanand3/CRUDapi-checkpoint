e0577c572b104adc3bd51132508143a286273803
aanand3 <as5265@columbia.edu>
2021-03-09 10:40:23 -0600

    made the getmapping, no tests yet

---
+++ src/test/java/com/example/demo/UserControllerTests.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import com.example.demo.user.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import javax.transaction.Transactional;
+
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTests
+{
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    UserRepository repo;
+
+    @Test
+    @Transactional
+    @Rollback
+    void getTheWholeTable() throws Exception
+    {
+        MockHttpServletRequestBuilder request = get("/users")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id", is(instanceOf(Number.class))));
+    }
+}
